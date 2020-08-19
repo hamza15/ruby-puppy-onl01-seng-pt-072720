3fc4f368602d2a3ff52eea8b1a6e50d6e0ec64ad
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-19 03:06:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Add your code here
+class Dog
+  @@all = []
+  
+  def initialize()
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @@all.each do |name|
+      puts "#{name}"
+    end
+  end 
+end
